0d931be38174c9fbc3505499cdbcaab969099b8e
ravi1312 <63062820+ravi1312@users.noreply.github.com>
2021-11-22 19:18:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask import render_template
-app = Flask(__name__,template_folder='webapp/templates')
+app = Flask(__name__,template_folder='templates')
 
 @app.route('/')
 def hello_world():
-        return render_template('home.html')
+        return render_template('base.html')
 if __name__ == "__main__":
         app.run(host='0.0.0.0',port=8080)
 
