20380a7c94a1c832bdc2d8535ed60922301c6dbd
ravi1312 <63062820+ravi1312@users.noreply.github.com>
2021-12-10 19:09:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__,template_folder='templates')
 @app.route('/')
 def hello_world():
         return render_template('home.html')
+@app.route('/payment')
+def base():
+    return render_template('payment.html')
 if __name__ == "__main__":
         app.run(host='0.0.0.0',port=8080)
 
