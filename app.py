1eb1e459bb25ac5fafabb5405eebfefd46d88d09
ravi1312 <63062820+ravi1312@users.noreply.github.com>
2021-12-10 20:22:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ def mobiles():
 def payment():
     return render_template('payment.html')
 if __name__ == "__main__":
-        app.run(host='0.0.0.0',port=8082)
+        app.run(host='0.0.0.0',port=8083)
 
 
